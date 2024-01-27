fb528655c4234017f444e6bf58609e780bb6cb87
Werner van der Merwe <wvdm1217@gmail.com>
2024-01-27 15:41:06 +0000

    feat: create CSVData class

--- invoice_gen/pull_csv_data.py
+++ invoice_gen/pull_csv_data.py
@@ -1,6 +1,36 @@
-"""
-Pulls data from csv file and returns a list of dictionaries
-"""
+import pandas as pd
+from typing import List, Dict, Any, Iterator
+import os
 
-import csv
+class CSVData:
+    """
+    Class to pull data from a csv file and returns a list of dictionaries
+    """
+    def __init__(self, input_file: str) -> None:
+        """
+        Initialize CSVData with the input file path.
+        
+        Args:
+            input_file (str): Path to the input CSV file.
+        """
+        if not os.path.isfile(input_file):
+            raise ValueError(f"{input_file} does not exist.")
+        if not input_file.endswith('.csv'):
+            raise ValueError(f"{input_file} is not a valid CSV file.")
+        self.data: List[Dict[str, Any]] = []
+        self.input_file = input_file
 
+    def read_csv(self) -> None:
+        """
+        Read the CSV data from the input file and store it in self.data.
+        """
+        self.data = pd.read_csv(self.input_file).to_dict('records')
+
+    def __iter__(self) -> Iterator[Dict[str, Any]]:
+        """
+        Returns the iterator object (self.data list)
+        
+        Returns:
+            Iterator[Dict[str, Any]]: An iterator over self.data
+        """
+        return iter(self.data)
