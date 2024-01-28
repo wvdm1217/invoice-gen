d60fae4b5fab5e8bedd1e9c9437bf69c342257b9
Werner van der Merwe <wvdm1217@gmail.com>
2024-01-28 17:35:50 +0000

    feat: add main program

--- invoice_gen/main.py
+++ invoice_gen/main.py
@@ -1,3 +1,16 @@
 """
 A program that generates pdf invoices from a csv file. 
 """
+from pull_csv_data import CSVData
+
+
+def main(csv_path="data/data.csv"):
+    print("This is the main function.")
+    csv_data = CSVData(csv_path)
+
+    for row in csv_data:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
